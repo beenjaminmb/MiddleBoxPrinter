5e042de9ce5e571e2cfed3f8f336078dcbeddff2
ben <bmixonb1@cs.unm.edu>
2016-07-15 16:52:58 -0700

    Changing from a doubly/singly linked list to a ring buffer for worker address lists.

--- util.c
+++ util.c
@@ -1,31 +1,84 @@
-#include "util.h"
+
+/* A replicating multiple producer/multiple consumer ring buffer.
+ *
+ * - A global head pointer points to the main head of the FIFO
+ * - A global tail pointer points to the NEXT FREE ITEM in FIFO
+ * - head == tail means the FIFO is empty - writing to the tail will fill in 
+ *   the head
+ * - tail + 1 == head means that the FIFO is full (yes, we waste one entry)
+ */
+
+#include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
-list_head_t *new_list() {
-  list_t *list = malloc(sizeof(list_t));
-  list->next = list->end = list;
-  list->data = NULL;
+#include <unistd.h>
+#include "util.h"
+
+int ringbuffer_full(ringbuffer_t *rb)
+{
+  ringbuffer_cell_t *next = rb->tail + 1;// because of how I call it.
+  if (next >= rb->end)
+    next = rb->buf;
+
+  return next == rb->head; // full buffer
 }
 
-int list_empty(list_head_t *head){
-  if (head) 
-    return (head->next == head);
-  else 
+int ringbuffer_empty(ringbuffer_t *rb)
+{
+  return (rb->head == rb->tail);
+}
+
+int Ringbuffer_append_single(ringbuffer_t *rb, void *item)
+{
+  ringbuffer_cell_t *next = rb->tail + 1;
+   
+  if (next >= rb->end)
+    next = rb->buf;
+
+  if (next == rb->head) // full buffer
     return -1;
+
+  rb->tail->value = item;
+  write_barrier(); /* Make sure that the item is set before tail
+		    * is updated by us - this is only needed in the 
+		    * lock-free case. If we use pthread locks and such,
+		    * the need for this goes away. */
+  rb->tail = next;
+  return 0;
 }
 
-int list_append(list_head_t *head, void *data) {
-  list_t *next = malloc(sizeof(list_t)); 
+int ringbuffer_remove_single(ringbuffer_t *rb, void **item)
+{
+  ringbuffer_cell_t *curr;
+
+  /* Check that the ring's not empty */
+  if (rb->head == rb->tail) // empty buffer
+    return -1;
+
+  /* get the item we want and advance the per-thread head. Beause
+   * this is all per-thread, there are no atomicity problems
+   * here. */
+  *item = rb->head->value;
+  curr = rb->head + 1;
+  if (curr >= rb->end)
+    rb->head = rb->buf;
+  else
+    rb->head = curr;
 
   return 0;
 }
 
-void list_destroy(list_head_t *list) {
-  list_t *current = list->next;
-  
-  while ( current ) {
-    
-  }
+int ringbuffer_destroy(ringbuffer_t *rb)
+{
+  if (rb->buf) free(rb->buf);
+  return 0;
 }
-void *list_remove(list_head_t *head) {
-  return NULL;
+
+/* Initialize a ringbuffer, giving the length in entries */
+int ringbuffer_init(ringbuffer_t *rb, int len)
+{
+  rb->head = rb->tail = rb->buf 
+    = malloc(len * sizeof(ringbuffer_cell_t));
+  rb->end = rb->buf+len;
+  return 0;
 }
--- util.h
+++ util.h
@@ -1,27 +1,27 @@
 #ifndef _UTIL_
 #define _UTIL_
 
+#include <pthread.h>
 
-typedef struct list_t {
-  struct list_t *next;
-  void* data;
-} list_t;
+#define memory_barrier()    asm volatile("mfence":::"memory")
+#define read_barrier()   asm volatile("lfence":::"memory")
+#define write_barrier()   asm volatile("sfence" ::: "memory")
 
+typedef struct ringbuffer_cell {
+  void *value;
+} ringbuffer_cell_t;
 
+typedef struct ringbuffer {
+  ringbuffer_cell_t *head, *tail;
+  ringbuffer_cell_t *buf, *end;
+} ringbuffer_t;
 
-typedef struct list_head_t {
-  list_t *head;
-  list_t *end;
-} list_head_t;
+int ringbuffer_innit(ringbuffer_t *rb, int len);
+int ringbuffer_destroy(ringbuffer_t *rb);
+int ringbuffer_append_single(ringbuffer_t *rb, void *item);
+int ringbuffer_remove_single(ringbuffer_t *rb, void **item);
+int ringbuffer_empty(ringbuffer_t *rb);
+int ringbuffer_full(ringbuffer_t *rb);
 
-list_t *new_list();
-
-void list_destroy(list_t *list);
-
-int list_empty(list_t *head);
-
-int list_append(list_t *head, void *data);
-
-void *list_remove(list_t *head);
 
 #endif /* _UTIL_ */
