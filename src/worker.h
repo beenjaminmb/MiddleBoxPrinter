1788d56240863b2d2cfd79f74ceb392c2930ceae
ben <bmixonb1@cs.unm.edu>
2016-08-15 13:04:41 -0600

    Moving code to src directory

--- src/worker.h
+++ src/worker.h
@@ -11,7 +11,7 @@
 #define CAPTURE_IFACE "wlan0"
 #define MAX_ADDR_SIZE sizeof("255.255.255.255\0")
 #define MTU 1500 /* Make size for a probe including payload*/
-#define NORMAL_MTU 576
+#define NORMAL_MTU 576 /* Official MTU of the Internet. */
 #define RATE 10000000.0 /* 1000 packets per second, 1 Gb/sec */
 #define PERIOD 60.0 /* Period in seconds == 1 minute */
 #define AVG_PACKET_SIZE 6000.0 /* 750 Bytes == 6000bits */
