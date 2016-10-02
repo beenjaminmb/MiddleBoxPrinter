f0b539d101eea0b75f99327e41e1d4e850091288
ben <bmixonb1@cs.unm.edu>
2016-10-01 20:02:42 -0600

    Reforactor and functionality additions

--- src/util.h
+++ src/util.h
@@ -15,7 +15,7 @@
 #define FALSE !TRUE
 #define DO_TCP(x) (x < 50)
 #define DO_UDP(x) ((x >= 50) & (x < 75))
-#define DO_ICMP(x) ((x > 74) & (x <= 100))
+#define DO_ICMP(x) ((x > 74) & (x <= 85))
 
 extern int errno;
 
@@ -49,32 +49,5 @@ static inline double wall_time()
   gettimeofday(&t, NULL);
   return 1. * t.tv_sec + 1.e-6 * t.tv_usec;
 }
-
-/* #define _RINGBUFFER 0 */
-
-/* #ifdef _RINGBUFFER */
-/* #define memory_barrier()    asm volatile("mfence":::"memory") */
-/* #define read_barrier()   asm volatile("lfence":::"memory") */
-/* #define write_barrier()   asm volatile("sfence" ::: "memory") */
-
-/* typedef struct ringbuffer_cell { */
-/*   void *value; */
-/* } ringbuffer_cell_t; */
-
-/* typedef struct ringbuffer { */
-/*   ringbuffer_cell_t *head, *tail; */
-/*   ringbuffer_cell_t *buf, *end; */
-/*   pthread_mutex_t *rb_lock; */
-/*   pthread_cond_t *rb_empty; */
-/*   pthread_cond_t *rb_full; */
-/* } ringbuffer_t; */
-
-/* int ringbuffer_init(ringbuffer_t *rb, int len); */
-/* int ringbuffer_destroy(ringbuffer_t *rb); */
-/* int ringbuffer_append_single(ringbuffer_t *rb, void *item); */
-/* int ringbuffer_remove_single(ringbuffer_t *rb, void **item); */
-/* int ringbuffer_empty(ringbuffer_t *rb); */
-/* int ringbuffer_full(ringbuffer_t *rb); */
-/* #endif */
 
 #endif /* _UTIL_ */
