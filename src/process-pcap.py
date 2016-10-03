300b0a75c6d300057fd8920245eda4e4cda4a571
ben <bmixonb1@cs.unm.edu>
2016-10-02 18:45:02 -0600

    Fixing KeyError

--- src/process-pcap.py
+++ src/process-pcap.py
@@ -17,21 +17,46 @@ def process_pcap(**kwargs):
                     dst = socket.inet_ntoa(ip.dst)
                     src = socket.inet_ntoa(ip.src)
                     ttl = ip.ttl
-                    # print dst, src, ttl
-                    if src == "64.106.82.6" and ((src, dst) not in data):
-                        data[(src, dst)] = [[], []]
-                    elif src == "64,106.82.6":
-                        data[(dst, src)][0] += [(ts, ttl)]
-                    elif dst == "64.106.82.6":
-                        if (dst, src) not in data:
-                            data[(dst, src)][1] = []
-                        data[(dst, src)][1] += [(ttl, ts)]
+                    if src == "64.106.82.6":
+                        print "Bar", src, dst
+                        if not ((src, dst) in data):
+                            data[(src, dst)] = [[(ts, ttl)], []]
+                        elif ((src, dst) in data):
+                            data[(src, dst)][0] += [(ttl, ts)]
+                        else:
+                            print "BooFar"
 
-            except Exception as e:
-                pass  # print e
+                    elif dst == "64.106.82.6":
+                        print "Foo", src, dst
+
+                    else:
+                        print "FUCK", src, dst
+                        # if src == map(ord, "64.106.82.6")) and not ((src, dst) in data):
+                        #     data[(src, dst)] = [[(ts, ttl)], []]
+
+                        # elif src == "64.106.82.6" and ((src, dst) in data):
+                        #     data[(dst, src)][0] += [(ts, ttl)]
+
+                        # if dst == "64.106.82.6":
+                        #     if not ((dst, src) in data):
+                        #         data[(dst, src)][1] = []
+                        #         data[(dst, src)][1] += [(ttl, ts)]
+                        #         print "if", src, dst
+                        #     elif (dst, src) in data:
+                        #         data[(dst, src)][1] += [(ttl, ts)]
+                        #         print "elif", src, dst
+                        #     else:
+                        #         print "else", src, dst
+                        # else:
+                        #     src, dst
+            except Exception as ex:
+                template = "An exception of type {0} occured. Arguments:\n{1!r}"
+                message = template.format(type(ex).__name__, ex.args)
+                print message, ttl, ts
     for (dst, src) in data:
-        d = data[(dst, src)]
-        print dst, src, "foo", d
+        d1 = data[(dst, src)][0]
+        d2 = data[(dst, src)][1]
+        print dst, src, d1, d2
 
 
 def main():
