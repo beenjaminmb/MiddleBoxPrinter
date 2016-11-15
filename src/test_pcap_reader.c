c074020289adc6017e4f2136c6f0f97a2dea291c
ben <bmixonb1@cs.unm.edu>
2016-11-14 22:56:07 -0700

    Testing stuff

--- src/test_pcap_reader.c
+++ src/test_pcap_reader.c
@@ -9,6 +9,12 @@ int test_parse_pcap()
   return 0;
 }
 
+int test_split_query_response()
+{
+  dict_t * split_query_response();
+  return 0;
+}
+
 int main(void)
 {
   assert( (test_parse_pcap() == 0));
